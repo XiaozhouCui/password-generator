9b3489a2fb3ef9d175364a6a3a650487ab4832ba
Joe Cui <joe.cui@bpanz.com>
2020-10-02 22:35:46 +1000

    Event binding and property binding syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+
+  onButtonClick() {
+    this.password = 'MY PASSWORD!';
+  }
+
+  getName() {
+    return 'Joe';
+  }
 }
